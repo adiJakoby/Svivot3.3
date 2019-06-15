7f0b82fd53eb64eabae729ca7c474f7dc529dc72
Dorintzalach <tzalach@post.bgu.ac.il>
2019-06-15 20:02:33 +0300

    finish logged in finally

--- pages/favorites/favoritesController.js
+++ pages/favorites/favoritesController.js
@@ -1,38 +1,54 @@
-angular.module('myApp').controller('favoritesController', function ($scope, $http) {
-    $scope.poiShow = false;
-
+angular.module('myApp').controller('favoritesController', function ($scope,$q, $http,$window) {
     let self = $scope;
-    self.points = [];
+    $scope.poiShow = false;
+    let savedPoints = [];
+    self.savedPoints = [];
 
-    sessionStorage.getItem('favorites');
+    let token = $window.sessionStorage.getItem("token");
 
-    $http({
-        method: "GET",
-        url: "http://localhost:3000/getAllPoints",
-    }).then(function (res) {
-        let i = 0;
-        let j = 0;
-        let k = 0;
-        for (j; j < res.data.length;) {
-            let threePoints = [];
-            for (i = 0; i < 3; i++) {
-                if(j < res.data.length){
-                    threePoints[i] = res.data[j];
-                    j++;
-                }
+    savedPoints = JSON.parse($window.sessionStorage.getItem("favorites"));
+
+    let i = 0;
+    let j = 0;
+    let k = 0;
+    for (j; j < savedPoints.length;) {
+        let fourPoints = [];
+        for (i = 0; i < 4; i++) {
+            if (j < savedPoints.length) {
+                fourPoints[i] = savedPoints[j];
+                j++;
             }
-            self.points[k] = threePoints;
-            k++;
         }
-
-        // self.points = res.data;
-
-    }, function (err) {
-        console.log(err)
-    })
+        self.savedPoints[k] = fourPoints;
+        k++;
+    }
+    // //get last two saved points
+    // $http({
+    //     method: "GET",
+    //     url: "http://localhost:3000/private/getSavedPoints",
+    //     headers: {
+    //         'x-auth-token': token,
+    //     }
+    // }).then(function (res) {
+    //     let i = 0;
+    //     let j = 0;
+    //     let k = 0;
+    //     for (j; j < res.data.length;) {
+    //         let threePoints = [];
+    //         for (i = 0; i < 3; i++) {
+    //             if (j < res.data.length) {
+    //                 threePoints[i] = res.data[j];
+    //                 j++;
+    //             }
+    //         }
+    //         self.points[k] = threePoints;
+    //         k++;
+    //     }
+    // }, function (err) {
+    //     console.log(err)
+    // })
 
     $scope.toShow = function (name) {
-
         $http({
             method: "GET",
             url: "http://localhost:3000/getPoint",
@@ -40,9 +56,8 @@ angular.module('myApp').controller('favoritesController', function ($scope, $htt
                 pointName: name
             }
         }).then(function (res) {
-            console.log(res.data);
             $scope.name = name;
-            $scope.imgSource = 'images/3.JPG';
+            $scope.imgSource = res.data[0].PICTURE;
             $scope.description = res.data[0].description;
             $scope.numOfViews = res.data[0].numofviews;
             $scope.rank = res.data[0].rank;
@@ -52,43 +67,70 @@ angular.module('myApp').controller('favoritesController', function ($scope, $htt
         }, function (err) {
             console.log(err)
         })
-
     }
+    $scope.sortByRank = function () {
 
-    $scope.filterByCategory = function (category) {
-        self.points = [];
+        /**
+         * Generic array sorting
+         *
+         * @param property
+         * @returns {Function}
+         */
+        var sortByProperty = function (property) {
+            return function (x, y) {
+                return ((x[property] === y[property]) ? 0 : ((x[property] < y[property]) ? 1 : -1));
+            };
+        };
 
-        $http({
-            method: "GET",
-            url: "http://localhost:3000/getPointsByCategory",
-            params: {
-                category: name
+        let i = 0;
+        let toSort = [];
+        var promises = [];
+
+        for (; i < self.savedPoints.length; i++) {
+            let name = self.savedPoints[i].NAME;
+            {
+                let sortPosition = i;
+                promises.push($http({
+                    method: "GET",
+                    url: "http://localhost:3000/getPoint",
+                    params: {
+                        pointName: name
+                    }
+                }).then(res => {
+                    res.data[0].NAME = name;
+                    toSort[sortPosition] = res.data[0];
+                }, function (err) {
+                    console.log(err)
+                }));
             }
-        }).then(function (res) {
-            let i = 0;
+        }
+
+        $q.all(promises).then(function () {
+            console.log(toSort);
+            toSort.sort(sortByProperty('rank'));
+            console.log("*****");
+            console.log(toSort);
+
+            self.points = [];
+            i = 0;
             let j = 0;
             let k = 0;
-            for (j; j < res.data.length;) {
-                let threePoints = [];
-                for (i = 0; i < 3; i++) {
-                    if(j < res.data.length){
-                        threePoints[i] = res.data[j];
+            for (j; j < toSort.length;) {
+                let fourPoints = [];
+                for (i = 0; i < 4; i++) {
+                    if (j < toSort.length) {
+                        toSort[j].PICTURE = "images/" + toSort[j].NAME + ".jpg";
+                        fourPoints[i] = toSort[j];
                         j++;
                     }
                 }
-                self.points[k] = threePoints;
+                self.savedPoints[k] = fourPoints;
                 k++;
             }
-
-            // self.points = res.data;
-
-        }, function (err) {
-            console.log(err)
         })
     }
-
-
-});
+})
+;
 
 
 
