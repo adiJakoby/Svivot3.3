a784f7516e8528491aa5dec94a333ac7d2a23b7e
adiJakoby <adij94@gmail.com>
2019-06-15 15:21:26 +0300

    hey

--- pages/searchPOI/searchPOIController.js
+++ pages/searchPOI/searchPOIController.js
@@ -1,13 +1,15 @@
-angular.module('myApp').controller('searchPOIController', function ($scope, $http) {
+angular.module('myApp').controller('searchPOIController', function ($scope, $q, $http) {
     $scope.poiShow = false;
 
     let self = $scope;
     self.points = [];
+    self.allPoints = [];
 
     $http({
         method: "GET",
         url: "http://localhost:3000/getAllPoints",
     }).then(function (res) {
+        self.allPoints = res.data;
         let i = 0;
         let j = 0;
         let k = 0;
@@ -40,7 +42,7 @@ angular.module('myApp').controller('searchPOIController', function ($scope, $htt
         }).then(function (res) {
             console.log(res.data);
             $scope.name = name;
-            $scope.imgSource = 'images/3.JPG';
+            $scope.imgSource = res.data[0].numofviews.PICTURE;
             $scope.description = res.data[0].description;
             $scope.numOfViews = res.data[0].numofviews;
             $scope.rank = res.data[0].rank;
@@ -58,11 +60,12 @@ angular.module('myApp').controller('searchPOIController', function ($scope, $htt
 
         $http({
             method: "GET",
-            url: "http://localhost:3000/getPointsByCategory",
+            url: "http://localhost:3000/getPointsByCatagory",
             params: {
-                category: name
+                category: category
             }
         }).then(function (res) {
+            self.allPoints = res.data;
             let i = 0;
             let j = 0;
             let k = 0;
@@ -85,6 +88,67 @@ angular.module('myApp').controller('searchPOIController', function ($scope, $htt
         })
     }
 
+    $scope.sort = function () {
+
+        /**
+         * Generic array sorting
+         *
+         * @param property
+         * @returns {Function}
+         */
+        var sortByProperty = function (property) {
+            return function (x, y) {
+                return ((x[property] === y[property]) ? 0 : ((x[property] < y[property]) ? 1 : -1));
+            };
+        };
+
+        let i = 0;
+        let toSort = [];
+        var promises = [];
+
+        for(; i < self.allPoints.length; i++){
+            let name = self.allPoints[i].NAME;
+            {
+                let sortPosition = i;
+                promises.push($http({
+                    method: "GET",
+                    url: "http://localhost:3000/getPoint",
+                    params: {
+                        pointName: name
+                    }
+                }).then(res => {
+                    res.data[0].NAME = name;
+                    toSort[sortPosition] = res.data[0];
+                }, function (err) {
+                    console.log(err)
+                }));
+            }
+        }
+
+        $q.all(promises).then(function () {
+            console.log(toSort);
+            toSort.sort(sortByProperty('rank'));
+            console.log("*****");
+            console.log(toSort);
+
+            self.points = [];
+            i = 0;
+            let j = 0;
+            let k = 0;
+            for (j; j < toSort.length;) {
+                let threePoints = [];
+                for (i = 0; i < 3; i++) {
+                    if(j < toSort.length){
+                        toSort[j].PICTURE = "images/" + toSort[j].NAME + ".jpg";
+                        threePoints[i] = toSort[j];
+                        j++;
+                    }
+                }
+                self.points[k] = threePoints;
+                k++;
+            }
+        })
+    }
 
 });
 
